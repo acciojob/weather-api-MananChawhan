a624a4869f808ecdad4f5915e0ade61ff42953c9
Manan Chawhan <mananchawhan.official@gmail.com>
2025-06-25 09:06:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If req
 document.getElementById("getWeatherBtn").addEventListener("click", () => {
   const apiKey = "e467712b257e418838be97cc881a71de"; // Must match what Cypress intercepts
-  const url = https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey};
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}`;
 
   fetch(url, { cache: "no-store" }) // prevents caching issues
     .then((response) => {
@@ -12,7 +12,7 @@ document.getElementById("getWeatherBtn").addEventListener("click", () => {
     })
     .then((data) => {
       const weatherDescription = data.weather[0].main;
-      document.getElementById("weatherData").innerText = Current weather in London: ${weatherDescription};
+      document.getElementById("weatherData").innerText = `Current weather in London: ${weatherDescription}`;
     })
     .catch((error) => {
       document.getElementById("weatherData").innerText = "Error fetching weather data.";
